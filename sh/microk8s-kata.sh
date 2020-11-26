a804d7d5669dfb4c90cc34ce80b98815726d1536
Didier Durand <durand.didier@gmail.com>
2020-11-26 05:14:00 +0100

    more resilient apt update

--- sh/microk8s-kata.sh
+++ sh/microk8s-kata.sh
@@ -123,7 +123,7 @@ then
 
       create_gce_instance "$KATA_INSTANCE" "$KATA_IMAGE"
       
-      gcloud compute ssh $KATA_INSTANCE --command="sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove  -y" --zone $GCP_ZONE --project=$GCP_PROJECT
+      gcloud compute ssh $KATA_INSTANCE --command='sudo rm -rf /var/lib/apt/lists/* && sudo apt update -y && (sudo apt upgrade -y && sudo apt upgrade -y) && sudo apt autoremove  -y' --zone $GCP_ZONE --project=$GCP_PROJECT
       gcloud compute ssh $KATA_INSTANCE --command='(sudo groupadd docker || true) && sudo usermod -a -G docker ${USER}'  --zone $GCP_ZONE --project=$GCP_PROJECT
       #gcloud compute ssh $KATA_INSTANCE --command='sudo groupadd docker && sudo usermod -a -G docker ${USER} && sudo groupadd microk8s && sudo usermod -a -G microk8s ${USER}'  --zone $GCP_ZONE --project=$GCP_PROJECT
       gcloud compute scp $0  $KATA_INSTANCE:$(basename $0) --zone $GCP_ZONE --project=$GCP_PROJECT
